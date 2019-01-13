76397d8d0a193c10762a586d128dea39de151e92
rmnbrtshk <rmnbrtshk@gmail.com>
2019-01-13 15:17:03 +0200

    fix first and second regexp

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var timeExp = /([0-1][0-9]|[0-2][0-3])\:([0-6]0|[0-5][0-9])/;
+var timeExp = /([0-1]\d|2[0-3]):([0-5]\d)/;
 var str = 'time 12:55';
 var str1 = 'time 44:55';
 var str2 = 'time 23:59';
@@ -13,17 +13,18 @@ console.log('time 24:01' + ' is ' + str3.match(timeExp));
 console.log('time 37:98' + ' is ' + str4.match(timeExp));
 console.log('time 00:00' + ' is ' + str6.match(timeExp));
 
-var expressions = '1+ 2, -1.2* 3.4, -3 / -6,, -3 +/ -6, -2 - 2, -1.2 * 3.4, 1#w+ 2d',
-  expressionsExp = /(\-?\d[.]\d|\-?\d)\s?[+-\/\*]\s?(\-?\d[.]\d|\-?\d)/;
+var expressions = '1+ 2, -11.2222* 2223.455555, -3 / -6,, -3 +/ -6, -2 - 2, -1.2 * 3.4, 1#w+ 2d, ff  11  +   44',
+  expressionsExp = /(-?\d*[.]?\d)\s*[+-\/*]\s*(-?\d*[.]?\d)/;
 
 var newExpressionsArr = expressions.split(', ');
 newExpressionsArr.forEach(function(item) {
   var res = item.search(expressionsExp),
-  dataInf = 'incorrect data';
+  dataInf = 'is not';
+
   if (res >= 0) {
-    dataInf = 'correct data';
+    dataInf = 'is';
   }
-    console.log(item + ' is ' + dataInf);
+  console.log(item + ' ' + dataInf + ' correct data');
   
 });
 
